625014ae89df06079e27a6ece2fcffdfd11902f1
遥远 <102892395+Yaoyaowanli@users.noreply.github.com>
2022-09-26 22:16:24 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/cpp_Code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_Code.dir/main.cpp.o"
+  "cpp_Code"
+  "cpp_Code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
